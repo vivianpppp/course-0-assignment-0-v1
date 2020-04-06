2bcfc7f221c698f4aa365010cf350836845eae41
vaibhav <vaibhav@therightquestion.dev>
2020-04-05 22:15:53 -0700

    Updates to all assignments.

---
+++ fix-code-0/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  cout<<"Hello "; cout <<"World!"; cout << endl;
+}
---
+++ fix-code-4/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  cout << "My GitHub username is [fill in your username here]!";
+}
