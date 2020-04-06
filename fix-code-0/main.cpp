63eaf6c1ff1ce24533f46dc100172a68704e2588
vivianpppp <chunying_su@hotmail.com>
2020-04-06 21:45:56 +0000

    Updating all questions

--- fix-code-0/main.cpp
+++ fix-code-0/main.cpp
@@ -3,7 +3,8 @@
 using namespace std;
 
 int main() {
-  cout << "Hello "; 
-  cout << "World!"; 
+  cout << "I love hot cheetos. My favorite animal is dogs. I'm excited to learn about coding!";
   cout << endl;
 }
+
+publish_all
